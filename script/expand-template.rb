1d5aabcc7b0cb167582d5e27f22154ac39721f50
Paul Betts <paul@paulbetts.org>
2014-03-31 22:53:24 -0700

    Fix the component names

--- script/expand-template.rb
+++ script/expand-template.rb
@@ -12,7 +12,8 @@ component_id = ARGV[1]
 d = Nokogiri::HTML(open("https://components.xamarin.com/view/#{component_id}"))
 
 component_version = d.css("h1 .version").text.strip
-component_title = d.css(".component-detail .description p").text
+component_title = d.css('.component-detail h1').first.text.gsub(component_version, '').strip
+component_summary = d.css(".component-detail .description p").text
 component_description = d.css(".component-about p").first.text
 component_author = d.css("p.publisher a").text.gsub("\r\n", '').strip
 
