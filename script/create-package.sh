cf31d6df3fc8193ad8965650ee470b1933a30e23
Paul Betts <paul@paulbetts.org>
2014-03-31 22:06:02 -0700

    Write some scripts to generate NuGet packages

---
+++ script/create-package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rootdir=$(dirname $0)/..
+tmpdir=`mktemp -d -t nupkg`
+packageid="$1"
+
+cp $rootdir/templates/README.txt "$tmpdir"
+$rootdir/script/expand-template.rb "$rootdir/templates/package.nuspec.erb" $packageid > "$tmpdir/$packageid.nuspec"
+
+mkdir -p "$tmpdir/build"
+$rootdir/script/expand-template.rb "$rootdir/templates/package.props.erb" $packageid > "$tmpdir/build/$packageid.props"
+
+mkdir -p "$rootdir/target"
+mono "$rootdir/script/NuGet.exe" pack "$tmpdir/$packageid.nuspec" -BasePath "$tmpdir"
+mv *.nupkg "$rootdir/target"
+
+rm -rf $tmpdir
---
+++ script/expand-template.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'nokogiri'
+require 'open-uri'
+require 'erb'
+
+def superchomp(str)
+  str.gsub(/^\s*/, '').gsub(/\s*$/, '')
+end
+
+component_id = ARGV[1]
+d = Nokogiri::HTML(open("https://components.xamarin.com/view/#{component_id}"))
+
+component_version = d.css("h1 .version").text.strip
+component_title = d.css(".component-detail .description p").text
+component_description = d.css(".component-about p").first.text
+component_author = d.css("p.publisher a").text.gsub("\r\n", '').strip
+
+erb = ERB.new(File.read(ARGV[0]))
+puts erb.result(binding)
+
+## Platform list
+#p d.css("ul.platforms li").map {|x| x.attributes['class'].value }
