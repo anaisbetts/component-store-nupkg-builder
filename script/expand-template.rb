9475a8d58aae40cac750f58c888d6699d71a4b65
Nicolas VERINAUD <nicolas@chloro.eu>
2014-07-02 08:53:09 +0200

    Add rxforxamarin to the package list. Fix script to replace & with and because NuGet pack fail with this char.

--- script/expand-template.rb
+++ script/expand-template.rb
@@ -13,10 +13,10 @@ d = Nokogiri::HTML(open("https://components.xamarin.com/view/#{component_id}"))
 
 component_version = d.css("h1 .version").text.strip
 component_icon = "http://components.xamarin.com/#{d.css("#sidebar img").first['src']}"
-component_title = d.css('.component-detail h1').first.text.gsub(component_version, '').strip
-component_summary = d.css(".component-detail .description p").text
-component_description = d.css(".component-about p").first.text
-component_author = d.css("p.publisher a").text.gsub("\r\n", '').strip
+component_title = d.css('.component-detail h1').first.text.gsub(component_version, '').strip.gsub("&", "and")
+component_summary = d.css(".component-detail .description p").text.gsub("&", "and")
+component_description = d.css(".component-about p").first.text.gsub("&", "and")
+component_author = d.css("p.publisher a").text.gsub("\r\n", '').strip.gsub("&", "and")
 
 erb = ERB.new(File.read(ARGV[0]))
 puts erb.result(binding)
